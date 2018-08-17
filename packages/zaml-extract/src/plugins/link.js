b426dad088d47f4957f446e2787f9598a579b02d
alphakevin <get.so.smart@gmail.com>
2018-08-17 11:04:49 +0800

    Fix incorrect import

--- packages/zaml-extract/src/plugins/link.js
+++ packages/zaml-extract/src/plugins/link.js
@@ -1,5 +1,4 @@
 import createLinkify from 'linkify-it';
-import { Promise } from 'core-js';
 
 const linkify = createLinkify();
 const P_EMAIL = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
