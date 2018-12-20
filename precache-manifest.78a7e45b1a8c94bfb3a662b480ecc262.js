29eff4c6f82213f3165327ab2f315d38091f04a2
alphakevin <get.so.smart@gmail.com>
2018-12-20 13:43:49 +0800

    Updates

---
+++ precache-manifest.78a7e45b1a8c94bfb3a662b480ecc262.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b94cec9419ec15be57a1bde1b579e4f9",
+    "url": "/zaml-lang/static/media/logo.b94cec94.svg"
+  },
+  {
+    "revision": "93ee00720e4e164d01de",
+    "url": "/zaml-lang/static/js/runtime~main.93ee0072.js"
+  },
+  {
+    "revision": "ae405c5ebd3ea866cb7d",
+    "url": "/zaml-lang/static/js/main.ae405c5e.chunk.js"
+  },
+  {
+    "revision": "af8daf31064caee9f9c2",
+    "url": "/zaml-lang/static/js/1.af8daf31.chunk.js"
+  },
+  {
+    "revision": "ae405c5ebd3ea866cb7d",
+    "url": "/zaml-lang/static/css/main.5b1fe174.chunk.css"
+  },
+  {
+    "revision": "af8daf31064caee9f9c2",
+    "url": "/zaml-lang/static/css/1.02148d34.chunk.css"
+  },
+  {
+    "revision": "60d86c90b68a8b0412282458b96d5853",
+    "url": "/zaml-lang/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/zaml-lang/precache-manifest.78a7e45b1a8c94bfb3a662b480ecc262.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/zaml-lang/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
