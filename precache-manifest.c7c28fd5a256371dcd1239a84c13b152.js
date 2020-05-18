728ea14815061f081646e3576e6969f93e1eaf08
alphakevin <get.so.smart@gmail.com>
2020-05-18 10:44:55 +0800

    Updates

---
+++ precache-manifest.c7c28fd5a256371dcd1239a84c13b152.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce199e50a37eb5605e0d972480a4708",
+    "url": "/zaml-lang/index.html"
+  },
+  {
+    "revision": "4fbebfaf7a641dfd8b69",
+    "url": "/zaml-lang/static/css/2.7f114a90.chunk.css"
+  },
+  {
+    "revision": "09312ce74d0909ea8bb5",
+    "url": "/zaml-lang/static/css/main.e01de196.chunk.css"
+  },
+  {
+    "revision": "4fbebfaf7a641dfd8b69",
+    "url": "/zaml-lang/static/js/2.d1b6995f.chunk.js"
+  },
+  {
+    "revision": "7f3924adf58b204138d47f471acaa46b",
+    "url": "/zaml-lang/static/js/2.d1b6995f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09312ce74d0909ea8bb5",
+    "url": "/zaml-lang/static/js/main.62545216.chunk.js"
+  },
+  {
+    "revision": "bc1c9337d463f87762d3",
+    "url": "/zaml-lang/static/js/runtime-main.c81c8f00.js"
+  },
+  {
+    "revision": "b94cec9419ec15be57a1bde1b579e4f9",
+    "url": "/zaml-lang/static/media/logo.b94cec94.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zaml-lang/precache-manifest.888c9ea2c61c5401d95a6c207bb36035.js"
+  "/zaml-lang/precache-manifest.c7c28fd5a256371dcd1239a84c13b152.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/zaml-lang/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zaml-lang/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
